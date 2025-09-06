a2379426cc06c87b1a4ca78b8faf406b605a6e2a
Krishni Agrahari <krishni@example.com>
2025-09-06 23:07:01 +0530

    First Commit

---
+++ Server/models/User.js
@@ -0,0 +1,23 @@
+﻿import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+
+        //structure of user db
+
+        _id: {type:String, required: true},//userid is stirng 
+        name: {type:String, required: true},  //userbame is string
+        email: {type:String, required: true},//user email is string
+        imageUrl: {type:String, required: true},
+        enrolledCourses: [//enrolled are formed array bcoz  that r also various parts
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Course'
+            }
+        ],
+    }, {timestamps: true}
+);
+//user model
+const User = mongoose.model('User', userSchema)
+
+export default User;
